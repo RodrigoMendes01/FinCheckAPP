5074e2bb6d176d998e6fa6db4da56932d1f245b7
Rodrigo <rodrigo.rm123.rm@gmail.com>
2023-12-12 14:44:15 -0300

    feat: add cors at main

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,6 +8,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://localhost:3001',
+  });
 
   await app.listen(3000);
 }
