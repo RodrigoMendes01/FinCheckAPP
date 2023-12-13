8471a64d9933456a8c272f3646f599e534933563
Rodrigo <rodrigo.rm123.rm@gmail.com>
2023-12-13 18:06:41 -0300

    feat: using forwardref at input and using clsx and tailwind-merge

--- web/src/view/components/Button.tsx
+++ web/src/view/components/Button.tsx
@@ -1,14 +1,15 @@
 import { ComponentProps } from 'react';
+import { cn } from '../../app/utils/cn';
 
 interface ButtonProps extends ComponentProps<'button'> {
   children: string
 }
 
-export function Button ({children, ...props}: ButtonProps) {
+export function Button ({children, className, ...props}: ButtonProps) {
   return (
     <button
       {...props}
-      className='bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 px-6 h-12 rounded-2xl font-medium text-white disabled:text-gray-400 disabled:cursor-not-allowed transition-all '
+      className={cn('bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 px-6 h-12 rounded-2xl font-medium text-white disabled:text-gray-400 disabled:cursor-not-allowed transition-all', className)}
     >
       {children}
     </button>
--- web/src/view/components/Input.tsx
+++ web/src/view/components/Input.tsx
@@ -1,26 +1,41 @@
-import { ComponentProps } from 'react';
+import { ComponentProps, forwardRef } from 'react';
+import { CrossCircledIcon } from '@radix-ui/react-icons';
+import { cn } from '../../app/utils/cn';
 
 interface InputProps extends ComponentProps<'input'> {
   name: string;
+  error?: string
 }
 
-export function Input ({placeholder, name, id, ...props}: InputProps) {
-  return (
-    <div className='relative'>
-      <input
-        {...props}
-        name={name}
-        id={id ?? name}
-        className='bg-white rounded-lg w-full border border-gray-500 px-3 h-[52px] text-gray-800 pt-4 peer placeholder-shown:pt-0 focus:border-gray-800 transition-all outline-none'
-        placeholder=' '
-      >
-      </input>
-      <label
-        className='absolute text-xs left-[13px] top-2 pointer-events-none text-gray-700 peer-placeholder-shown:text-base peer-placeholder-shown:top-3.5 transition-all'
-        htmlFor={id ?? name}
-      >
-        {placeholder}
-      </label>
-    </div>
-  );
-}
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({placeholder, name, id, error, className, ...props}, ref) => {
+    const inputId = id ?? name;
+
+    return (
+      <div className='relative'>
+        <input
+          {...props}
+          name={name}
+          ref={ref}
+          id={inputId}
+          placeholder=' '
+          className={cn('bg-white rounded-lg w-full border border-gray-500 px-3 h-[52px] text-gray-800 pt-4 peer placeholder-shown:pt-0 focus:border-gray-800 transition-all outline-none', error && '!border-red-900', className
+          )}
+        >
+        </input>
+        <label
+          className='absolute text-xs left-[13px] top-2 pointer-events-none text-gray-700 peer-placeholder-shown:text-base peer-placeholder-shown:top-3.5 transition-all'
+          htmlFor={inputId}
+        >
+          {placeholder}
+        </label>
+
+        {error && (
+          <div className='flex gap-2 items-center mt-2 text-red-900'>
+            <CrossCircledIcon/>
+            <span className='text-xs'>{error}</span>
+          </div>
+        )}
+      </div>
+    );
+  });
