04dc4495e55a144116080edd0412ef8c31260cef
Rodrigo <rodrigo.rm123.rm@gmail.com>
2023-12-14 17:57:39 -0300

    doc: add svg as components

--- web/src/view/pages/DashBoard/index.tsx
+++ web/src/view/pages/DashBoard/index.tsx
@@ -1,15 +1,25 @@
-import { useAuth } from '../../../app/hooks/useAuth';
-import { Button } from '../../components/Button';
+import { Logo } from '../../components/Logo';
+import { UserMenu } from '../../components/UserMenu';
+import { Accounts } from './components/Accounts';
+import { Transactions } from './components/Transactions';
 
 export function DashBoard() {
-  const { signout } = useAuth();
-
   return (
-    <>
-      <h1>DASHBOARD PAGE</h1>
-      <Button onClick={signout}>
-        Sair
-      </Button>
-    </>
+    <div className='w-full h-full p-4 md:px-8 md:pb-8 md:pt-6 flex flex-col'>
+      <header className='h-12 flex justify-between'>
+        <Logo className='h-6 text-teal-900'/>
+        <UserMenu/>
+      </header>
+
+      <main className='mt-4 flex-1 flex flex-col gap-4 md:flex-row'>
+        <div className='w-full md:w-1/2'>
+          <Accounts/>
+        </div>
+
+        <div className='w-full md:w-1/2'>
+          <Transactions/>
+        </div>
+      </main>
+    </div>
   );
 }
