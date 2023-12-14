1f1af881ad95de58e4ae7582250479ed8b82a60f
Rodrigo <rodrigo.rm123.rm@gmail.com>
2023-12-14 14:24:16 -0300

    raw: updating configs of project

--- api/src/main.ts
+++ api/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: '*',
   });
 
   await app.listen(3000);
