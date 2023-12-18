7c11e236d45943916f645e5e423a2cbf06a0e321
Rodrigo <rodrigo.rm123.rm@gmail.com>
2023-12-18 13:55:48 -0300

    feat: implementing empty state in transactions

--- web/src/view/pages/DashBoard/components/Transactions/index.tsx
+++ web/src/view/pages/DashBoard/components/Transactions/index.tsx
@@ -8,20 +8,22 @@ import { SliderNavigation } from './SliderNavigation';
 import { Card } from './Card';
 import { useTransactionsController } from './useTransactionsController';
 import { Spinner } from '../../../../components/Spinner';
+import empytState from '../../../../../assets/images/empytState.svg';
 
 export function Transactions () {
-  const { isLoading } = useTransactionsController();
+  const { isInitialLoading, isLoading, transactions } = useTransactionsController();
+  const hasTransactions = transactions.length > 0;
 
   return (
     <div className="bg-gray-100 rounded-2xl w-full h-full p-10 flex flex-col">
-      {isLoading && (
+      {isInitialLoading && (
         <div className='w-full h-full flex justify-center items-center'>
           <Spinner/>
         </div>
       )}
 
 
-      {!isLoading && (
+      {!isInitialLoading && (
         <>
           <header>
             <div className='flex justify-between items-center'>
@@ -35,32 +37,48 @@ export function Transactions () {
                 <FilterIcon/>
               </button>
             </div>
+
+            <div className="mt-6 relative">
+              <Swiper
+                slidesPerView={3}
+                centeredSlides
+              >
+                <SliderNavigation/>
+
+                {Months.map((month, index) => (
+                  <SwiperSlide key={month}>
+                    {({ isActive }) => (
+                      <SliderMonths
+                        isActive={isActive}
+                        month={month}
+                        index={index}
+                      />
+                    )}
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
           </header>
 
-          <div className="mt-6 relative">
-            <Swiper
-              slidesPerView={3}
-              centeredSlides
-            >
-              <SliderNavigation/>
+          <div className="mt-4 space-y-2 flex-1 overflow-y-auto">
+            {isLoading && (
+              <div className='h-full flex justify-center items-center'>
+                <Spinner/>
+              </div>
+            )}
 
-              {Months.map((month, index) => (
-                <SwiperSlide key={month}>
-                  {({ isActive }) => (
-                    <SliderMonths
-                      isActive={isActive}
-                      month={month}
-                      index={index}
-                    />
-                  )}
-                </SwiperSlide>
-              ))}
-            </Swiper>
+            {(!hasTransactions && !isLoading) && (
+              <div className='h-full flex flex-col items-center justify-center'>
+                <img src={empytState} alt="Mulher com lupa" />
+                <span className='text-gray-700'>Não encontramos nenhuma transação</span>
+              </div>
+            )}
+
+            {(hasTransactions && !isLoading) && (
+              <Card/>
+            )}
           </div>
 
-          <div className="mt-4 space-y-2 flex-1">
-            <Card/>
-          </div>
         </>
       )}
     </div>
