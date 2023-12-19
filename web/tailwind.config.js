b8b713323141d085d98fc40222ce8f65ba89f53b
Rodrigo <rodrigo.rm123.rm@gmail.com>
2023-12-19 11:16:44 -0300

    feat: create modal component

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -185,11 +185,21 @@ export default {
         slideDownAndFade: {
           from: { opacity: 0, transaform: 'translateY(-2px)' },
           to: {opacity: 1, transaform: 'translateY(0)'}
+        },
+        overlayShow: {
+          from: { opacity: 0 },
+          to: { opacity: 1 }
+        },
+        contentShow: {
+          from: { opacity: 0, transaform: 'translate(-50%, -48%) scale(0.96)' },
+          to: { opacity: 1, transaform: 'translate(-50%, -50%) scale(1)' }
         }
       },
       animation: {
         slideUpAndFade: 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
-        slideDownAndFade: 'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)'
+        slideDownAndFade: 'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)'
       }
     },
   },
