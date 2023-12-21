770e32b9541be6338d22afc8ac394fd9c01c6bc1
Rodrigo <rodrigo.rm123.rm@gmail.com>
2023-12-21 16:07:43 -0300

    feat: update hooks for transaction and bank accounts

--- web/src/app/hooks/useBankAccounts.ts
+++ web/src/app/hooks/useBankAccounts.ts
@@ -4,7 +4,8 @@ import { bankAccountService } from '../services/bankAccountService';
 export function useBankAccounts () {
   const { data, isFetching } = useQuery({
     queryKey: ['bankAccounts'],
-    queryFn: bankAccountService.getAll
+    queryFn: bankAccountService.getAll,
+    staleTime: Infinity
   });
 
   return {
--- web/src/app/hooks/useTransactions.ts
+++ web/src/app/hooks/useTransactions.ts
@@ -1,19 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { transactionsService } from '../services/transactionsService';
+import { TransactionsFilters } from '../services/transactionsService/getAll';
 
-export function useTransactions () {
-  const { data, isFetching, isInitialLoading  } = useQuery({
+export function useTransactions (filters: TransactionsFilters) {
+  const { data, isFetching, isInitialLoading, refetch  } = useQuery({
     queryKey: ['transactions'],
-    queryFn: () => transactionsService.getAll({
-      month: 11,
-      year: 2023
-    })
+    queryFn: () => transactionsService.getAll(filters)
   });
 
   return {
     transactions: data ?? [],
     isLoading: isFetching,
-    isInitialLoading
+    isInitialLoading,
+    refetch
   };
 }
